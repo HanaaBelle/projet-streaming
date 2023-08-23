f77d51e1c27622d804226a21e29831b2c97ece7e
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-23 16:17:03 +0200

    Added paragraph composant & episode object(dictionnary) with title, duration, hasBeenWatched episode : with dot notation(notation pointée)

--- script_streaming.js
+++ script_streaming.js
@@ -6,7 +6,11 @@ let paragraph = document.querySelector('#info');
 let numberOfSeasons = 6;
 let numberOfEpisodes = 12 ;
 
-
+let episode = {
+    title: "The Original",
+    duration: 68,
+    hasBeenWatched: false,
+};
 
 
 // ==========================================
@@ -14,3 +18,7 @@ let numberOfEpisodes = 12 ;
 // Affecter au contenu du text de paragraph les valeurs suivantes : nombres de "saisons" et "épisodes"
 paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
 
+// Affecter à la balise <p> à l'id "#episode-info" les valeurs de l'objet (dictionnaire) : "épisode"
+document.querySelector('#episode-info').innerText = `Episode: ${episode.title}
+Duration: ${episode.duration} min
+${episode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
